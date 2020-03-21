3811a088e00ef17b3619e93bef7c34e27ccba3d4
sef202 <samflinkfelt@gmail.com>
2020-03-21 15:14:56 -0700

    Updated Scheduler
    Updated Scheduler

---
+++ lib/authenticate/user_model.dart
@@ -0,0 +1,11 @@
+// File defines a user model class to associate properties with a Firebase user ID
+
+class User {
+
+  // user property: uid - Firebase User ID
+  final String uid;
+
+  // sets uid property
+  User({ this.uid });
+
+}
