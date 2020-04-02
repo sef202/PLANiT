e1e6537cc468209cc792b2fe2eadb88e63ffe0dc
lidelmendoza <lidelmendoza9@csu.fullerton.edu>
2020-04-02 16:33:01 -0700

    Add files via upload

---
+++ lib/authenticate/user_class_model.dart
@@ -0,0 +1,16 @@
+
+class UserClassData {
+
+  final String uid;
+  final String className;
+  final String description;
+  final String day;
+  final String startTime;
+  final String endTime;
+  final String room;
+  final String instructor;
+
+  UserClassData({this.uid, this.className, this.description, this.day, this.startTime, this.endTime, this.room, this.instructor});
+
+
+}
