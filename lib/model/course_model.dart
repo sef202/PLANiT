336d08fc4f71e24253163566196009cf51a5f28a
lidelmendoza <lidelmendoza9@csu.fullerton.edu>
2020-04-02 16:34:53 -0700

    userClassData

---
+++ lib/model/course_model.dart
@@ -0,0 +1,14 @@
+class Course {
+
+final String User;
+final String className;
+final String description;
+final String day;
+final String startTime;
+final String endTime;
+final String room;
+final String instructor;
+final bool done;
+
+Course({this.User, this.className, this.description, this.day, this.startTime, this.endTime, this.room, this.instructor, this.done});
+}
